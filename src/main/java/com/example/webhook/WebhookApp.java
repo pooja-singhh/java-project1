72eb2b961eb26ae5835d2db7104f82f27b995c9d
Pooja Singh <poojasingh8246@gmail.com>
2025-09-04 00:10:28 +0530

    Update WebhookApp.java

--- src/main/java/com/example/webhook/WebhookApp.java
+++ src/main/java/com/example/webhook/WebhookApp.java
@@ -18,7 +18,7 @@ public class WebhookApp implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         webhookService.executeFlow();
     }
 }
